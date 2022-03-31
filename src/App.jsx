a40761a7082273ff9057eaf08f026f52955c6539
SergioDiaz10 <sergi0r0ladiaz15@gmail.com>
2022-03-31 16:16:25 -0600

    act_contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-function App() {
+import { useState } from "react";
+import "./app.css";
+
+const App = () => {
+  const [valor,setValor] = useState(0);
+  
+  const handleClick = (v) => {
+    console.log(v);
+    setValor(v + 1)
+  };
+
   return (
     <div className="App">
-      <h1>BIENVENIDO CONTADOR</h1>
+      <h3>contador</h3>
+      <hr />
+      <h2 className="text-center">{valor}</h2>
+      <hr />
+      <div className="margin-button">
+        <button className="btn btn-primary me-2" onClick={() => handleClick(valor)}>
+          +1
+        </button>
+      </div>
     </div>
   );
 }
